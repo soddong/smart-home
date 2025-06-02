84f8fa7dd915428b558b4161c082b2bbc6ec4407
Inwook Hwang <cafeterrace3750@gmail.com>
2025-06-02 19:45:46 +0900

    add certificate for TLS

--- device-fw/SmartHome/SmartHome.ino
+++ device-fw/SmartHome/SmartHome.ino
@@ -9,6 +9,8 @@
 /* Include for WiFi */
 #include <WiFi.h>
 #include <PubSubClient.h>
+#include "WiFiSSLClient.h"
+#include "Secret.h"
 
 /* Pin Info */
 #define P_IR_Receiver   8
@@ -17,37 +19,38 @@
 #define P_IR_Sender_SW  11
 
 /* Define Macros */
-#define IR_DECODE_MODE  false
-#define MQTT_PORT       1883
+#define MQTT_PORT       8883
 #define MQTT_BUF_SIZE   50
+#define IR_DECODE_MODE  false
 
 /* Global Variables */
-AM2302::AM2302_Sensor TempHum{P_TempHum};
-WiFiClient espClient;
+WiFiSSLClient espClient;
 PubSubClient mqttClient(espClient);
+AM2302::AM2302_Sensor TempHum{P_TempHum};
 
-volatile int consol_debug_cnt;
 volatile int tmp;
 volatile int hum;
+volatile bool isAcOn = false;
 
-const char* ssid = "soddong2";
-const char* password = "tvxq1226";
-const char* mqtt_server = "broker.mqtt-dashboard.com";
+const char* ssid        = SECRET_SSID;
+const char* password    = SECRET_PASS;
+const char* mqtt_server = SECRET_SERV;
 
-volatile int value;
+char msg[MQTT_BUF_SIZE];
 volatile long lastMsg_MQTT;
 volatile long lastMsg_TMPHUM;
-char msg[MQTT_BUF_SIZE];
+volatile long lastMsg_IRSignal;
 
 /* Function Prototypes */
 void IR_Receiver_Main(void);
-void IR_Sender_Main(const uint16_t[]);
+void IR_Sender_On(void);
+void IR_Sender_Off(void);
+void IR_Sender_Main(void);
 void TempHum_Main(int);
 void MQTT_Init(void);
 void MQTT_Main(void);
 void MQTT_Reconnect(void);
 void MQTT_Receive(char*, byte*, unsigned int);
-
 void print_raw_data(IRrecv);
 void handleOverflow();
 
@@ -60,33 +63,32 @@ void setup() {
   while(!Serial);
 
   /* Enable sensors with pin info */
+  TempHum.begin();
   IrSender.begin(P_IR_Sender);
   IrReceiver.begin(P_IR_Receiver);
-  TempHum.begin();
 
   /* Setup MQTT */
   MQTT_Init();
 
   /* Print IR Info */
   printActiveIRProtocols(&Serial);
+  Serial.println("");
 }
 
 void loop() {
   /* Main function for IR Receiver(Decoder) */
   #if IR_DECODE_MODE
   IR_Receiver_Main();
-  #endif
-
-  /* Main function for Temp-Hum Sensor*/
-  TempHum_Main(2000);
-
+  #else
   /* Main function for IR Sender */
   if(digitalRead(P_IR_Sender_SW) == LOW){
-    IR_Sender_Main(decoded_on_data_17_raw);
+    IR_Sender_Main();
   }
-
+  /* Main function for Temp-Hum Sensor */
+  TempHum_Main(2000);
   /* Main function for MQTT */
   MQTT_Main();
+  #endif
 }
 
 void IR_Receiver_Main(void){
@@ -107,10 +109,32 @@ void IR_Receiver_Main(void){
   }
 }
 
-void IR_Sender_Main(const uint16_t ir_signal[]){
-  /* Send Signal with 38KHz */
-  Serial.println(F("Sending Carrier IR signal..."));
-  IrSender.sendRaw(ir_signal, sizeof(ir_signal) / sizeof(ir_signal[0]), NEC_KHZ);
+void IR_Sender_On(void){
+  Serial.println(F("Send Turn On Signal"));
+  IrSender.sendRaw(decoded_on_data_17_raw2, 
+                   sizeof(decoded_on_data_17_raw2) / sizeof(decoded_on_data_17_raw2[0]), 
+                   NEC_KHZ);
+  isAcOn = true;
+}
+
+void IR_Sender_Off(void){
+  Serial.println(F("Send Turn Off Signal"));
+  IrSender.sendRaw(decoded_off_data_17_raw,
+                   sizeof(decoded_off_data_17_raw) / sizeof(decoded_off_data_17_raw[0]), 
+                   NEC_KHZ);
+  isAcOn = false;
+}
+
+void IR_Sender_Main(void){
+  unsigned long now = millis();
+  if (now - lastMsg_IRSignal > 2000){
+    lastMsg_IRSignal = now;
+    if(isAcOn == false){
+      IR_Sender_On();
+    }else{
+      IR_Sender_Off();
+    }
+  } 
 }
 
 void TempHum_Main(int sampling_period){
@@ -144,13 +168,16 @@ void MQTT_Init(void){
     Serial.print(F("."));
   }
 
+  /* Setup Certificate for TLS */
+  espClient.setCACert(ca_cert);
+
   /* Get seed from current time */
   randomSeed(micros());
 
   /* Print WiFi Info */
   Serial.println("");
   Serial.println(F("WiFi connected"));
-  Serial.println(F("IP address: "));
+  Serial.print(F("IP address: "));
   Serial.println(WiFi.localIP());
 
   /* Setup MQTT as Client */
@@ -169,11 +196,10 @@ void MQTT_Main(void){
   volatile unsigned long now = millis();
   if (now - lastMsg_MQTT > 2000) {
     lastMsg_MQTT = now;
-    ++value;
     snprintf (msg, MQTT_BUF_SIZE, "Current Temp Hum : #%d, #%d", tmp, hum);
     Serial.print(F("Publish message: "));
     Serial.println(F(msg));
-    mqttClient.publish("Starry/Test/Pub", msg);
+    mqttClient.publish("Starry/Pub", msg);
   }
 }
 
@@ -187,9 +213,9 @@ void MQTT_Receive(char* topic, byte* payload, unsigned int length){
   Serial.println("");
 
   if ((char)payload[0] == '1') {
-    /* Do something... */
+    IR_Sender_On();
   } else {
-    /* Do something... */
+    IR_Sender_Off();
   }
 }
 
@@ -197,20 +223,23 @@ void MQTT_Reconnect(void) {
   // Loop until we're reconnected
   while (!mqttClient.connected()) {
     Serial.print(F("Attempting MQTT connection..."));
-    // Create a random client ID
-    String clientId = "Client-";
-    clientId += String(random(0xffff), HEX);
-    // Attempt to connect
-    if (mqttClient.connect(clientId.c_str())) {
+    // Create Random Client Account
+    String clientName = "Client_";
+    clientName += String(random(0xffff), HEX);
+
+    const char* clientId = MQTT_CLIENT_ID;
+    const char* clientPw = MQTT_CLIENT_PW;
+
+    if (mqttClient.connect(clientName.c_str(), clientId, clientPw)) {
       Serial.println(F("connected"));
       // Once connected, publish an announcement...
-      mqttClient.publish("Starry/Test/Pub", "Reconnect");
+      mqttClient.publish("Starry/Pub", "Reconnect");
       // ... and resubscribe
-      mqttClient.subscribe("Starry/Test/Sub");
+      mqttClient.subscribe("Starry/Sub");
     } else {
       Serial.print("failed, rc=");
       Serial.print(mqttClient.state());
-      Serial.println(F("try again"));
+      Serial.println(F(", try again"));
     }
   }
 }
